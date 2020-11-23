352439d59279e3f5cb7a46696d3c2e4105f1e8ce
sebus208 <kovewebdesign@gmail.com>
2020-11-24 00:18:47 +0100

    The loop
    Implemented first version of the loop. Once logged in the bundle status
    value is checked and refreshed. Once it is below the minimum value for
    bundle status the MB Aanvuller page is opened in order to Bevestig a data
    Top Up. Once this is completed repeat the process.

--- topUpData.py
+++ topUpData.py
@@ -9,46 +9,56 @@ MB_MINIMUM = 200
 
 browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
 browser.get('https://www.t-mobile.nl/login')
+browser.maximize_window()
 
 try:
 	browser.find_element_by_css_selector('label[for=Row1_Column1_Cell1_CookieSettings_SelectedCookieTypeAnalytics]').click()
 except:
 	print("Cookie notice button not found")
+	browser.quit()
 
 try:
 	browser.find_element_by_id('Section1_Row2_Column1_Cell1_AutoLogin_AutoLoginButton').click()
 except:
 	print("Auto Login button not found")
+	browser.quit()
+
+browser.get('https://www.t-mobile.nl/my/company/verbruik-en-kosten')
 
 try:
-	bundleStatus = WebDriverWait(browser, 10).until(
-		EC.presence_of_element_located((By.CLASS_NAME, 'bundle-status-unit-value'))
+	browser.find_element_by_id('Row1_Column1_Cell1_SubscriberCallStatusOverview_ChanageButton').click()
+except:
+	print("Bekijk verbruik button not found")
+	browser.quit()
+
+try:
+	el = WebDriverWait(browser, 10).until(
+		EC.presence_of_element_located((By.ID, 'bundle-status-identifier'))
 	)
 except:
-	print("Page not loading")
-finally:
-	bundleStatusHtml = browser.find_element_by_css_selector('.bundle-status-unit-value').get_attribute('innerHTML')
-	bundleStatusValue = int(re.sub('[\D*]', '', bundleStatusHtml))
-
-if bundleStatusValue < MB_MINIMUM:
-	browser.get('https://www.t-mobile.nl/my/company/mbaanvullers')
-
-	try:
-		browser.find_element_by_id('Row1_Column1_Cell1_SubscriberCallStatusOverview_ChanageButton').click()
-	except:
-		print("Something went wrong opening the aanvuller page")
-
-else:
-	print("There is enough data...")
+	print("Bundle Status page not loading")
 	browser.quit()
+
+bundleStatusHtml = browser.find_element_by_css_selector('.bundle-status-unit-value').get_attribute('innerHTML')
+bundleStatusValue = int(re.sub('[\D*]', '', bundleStatusHtml))
+
+while bundleStatusValue >= 200:
+	browser.find_element_by_id('RefreshUsageButton').click()
+	browser.implicitly_wait(15)
+
+browser.get('https://www.t-mobile.nl/my/company/mbaanvullertoevoegen')
 
 try:
 	bundleTopUp = WebDriverWait(browser, 10).until(
 		EC.presence_of_element_located((By.ID, 'ZoneBundleOverview6_details'))
 	)
 except:
-	print("Page not loading")
-finally:
+	print("MB Aanvuller page not loading")
+
+try:
 	# browser.find_element_by_css_selector('#ZoneBundleOverview6_details input[value=Bevestigen]').click
-	print('click #ZoneBundleOverview6_details input[value=Bevestigen]')
-	browser.quit()
+	print('Click Bevestigen')
+	browser.implicitly_wait(5)
+	browser.get('https://www.t-mobile.nl/my/company/verbruik-en-kosten')
+except:
+	print("Auto Login button not found")
