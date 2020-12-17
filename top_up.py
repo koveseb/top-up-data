b3280172c844f5660b5b1c51751646d54ebfb620
sebus208 <kovewebdesign@gmail.com>
2020-12-17 23:00:40 +0100

    Use mifi admin
    Started refactoring in order to use the mifi admin page to track how
    many MB's have been used. Once the maximum has been reached the bot
    opens a new window and starts the mb aanvuller procedure.

--- top_up.py
+++ top_up.py
@@ -6,124 +6,185 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+CHROME_PATH = "/home/kove/code/python/drivers/chromedriver"
+
 LOGIN = "https://www.t-mobile.nl/login"
 VERBRUIK = "https://www.t-mobile.nl/my/company/verbruik-en-kosten"
+OVERVIEW = "https://www.t-mobile.nl/my/company/mbaanvullers"
 AANVULLERS = "https://www.t-mobile.nl/my/company/mbaanvullertoevoegen"
+MIFI = "http://mifi.local"
+STATISTIC = MIFI + "/html/statistic.html"
 
-MB_MINIMUM = 215
-REFRESHRATE = 120
+MB_MAX = 800
+REFRESHRATE = 10
 
-def reset():
-    i = 0
 
-    browser.quit()
+## TODO Send desktop notifications when bot succeeds and when it fails
+## TODO Fine tune all the try except blocks
 
-    browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
-    browser.get(LOGIN)
-    browser.maximize_window()
+
+def notFound(element):
+    return element + " not found"
+
+
+def loginTmobile():
+    b2 = webdriver.Chrome(CHROME_PATH)
+    b2.get(LOGIN)
+    b2.maximize_window()
+
+    no_cookies = (
+        "label[for=Row1_Column1_Cell1_CookieSettings_SelectedCookieTypeAnalytics]"
+    )
+    auto_login = "Section1_Row2_Column1_Cell1_AutoLogin_AutoLoginButton"
 
     try:
-        browser.find_element_by_css_selector(
-            "label[for=Row1_Column1_Cell1_CookieSettings_SelectedCookieTypeAnalytics]"
-        ).click()
+        b2.find_element_by_css_selector("no_cookies").click()
     except:
-        print("Cookie notice button not found")
-        browser.quit()
+        print(notFound(no_cookies))
 
     try:
-        browser.find_element_by_id("Section1_Row2_Column1_Cell1_AutoLogin_AutoLoginButton").click()
+        b2.find_element_by_id().click()
     except:
-        print("Auto Login button not found")
-        browser.quit()
-    else:
-        bekijkVerbruik()
+        print(notFound(auto_login))
+
+    return b2
 
 
+def reset(b2):
+    b2.get(AANVULLERS)
+
+
+# TODO Figure out best way to loop mbsAanvullen if the page doesn't load properly
 def mbsAanvullen():
-    browser.get(AANVULLERS)
+    b2 = loginTmobile()
+
+    b2.get(OVERVIEW)
+    aanvullen_overview = (
+        "Section1_Row1_Column1_Cell1_SubscriberCallStatusOverview_ChanageButton"
+    )
 
     try:
-        bundleTopUp = WebDriverWait(browser, 20).until(
-            EC.presence_of_element_located((By.ID, "buyBundleModal_A0DAY02"))
+        b2.find_element_by_id(aanvullen_overview).click()
+    except:
+        print(notFound(aanvullen_overview))
+
+    bundle_modal = "buyBundleModal_A0DAY02"
+    select_bundle = bundle_modal + " .button-green.button-small.button-block"
+    buy_bundle = select_bundle + ".mb-2.mb-tablet-0.order-tablet-last.buyBundleButton"
+    try:
+        WebDriverWait(b2, 20).until(
+            EC.presence_of_element_located((By.ID, bundle_modal))
         )
     except:
-        print("MB Aanvuller page not loading")
+        print(notFound(bundle_modal))
 
     try:
-        browser.execute_script("jQuery('.list-group-inset[data-interaction-id=bundle_for_unlimited] li:not(:last-of-type)').remove()")
-        browser.execute_script("jQuery('#buyBundleModal_A0DAY02').css('height', '0')")
-        browser.find_element_by_css_selector('#buyBundleModal_A0DAY02 .button-green.button-small.button-block').click()
-        browser.execute_script("jQuery('#buyBundleModal_A0DAY02').css('height', 'auto')")
+        b2.execute_script(
+            "jQuery('.list-group-inset[data-interaction-id=bundle_for_unlimited] li:not(:last-of-type)').remove()"
+        )
+        b2.execute_script("jQuery('" + bundle_modal + "').css('height', '0')")
+        b2.find_element_by_css_selector(select_bundle).click()
+        b2.execute_script("jQuery('" + bundle_modal + "').css('height', 'auto')")
         time.sleep(3)
-        browser.find_element_by_css_selector('#buyBundleModal_A0DAY02 .button-green.button-block.mb-2.mb-tablet-0.order-tablet-last.buyBundleButton').click()
+        b2.find_element_by_css_selector(buy_bundle).click()
         print("Bevestigen geclicked")
         time.sleep(420)
 
-        bekijkVerbruik()
+        return True
     except:
-        mbsAanvullen()
+        reset(b2)
 
-def bekijkVerbruik():
-    browser.get(VERBRUIK)
+
+def loginMifi():
+    b1 = webdriver.Chrome(CHROME_PATH)
+    b1.get(MIFI)
+    b1.maximize_window()
+    login = "logout_span"
+    username_field = "username"
+    password_field = "password"
+    confirm = "pop_login"
+    username = "admin"
+    password = "9z$Y1kLL6E8k"
+    statistic = "statistic"
 
     try:
-        browser.find_element_by_id(
-            "Row1_Column1_Cell1_SubscriberCallStatusOverview_ChanageButton"
-        ).click()
+        b1.find_element_by_id(login).click()
     except:
-        print("Bekijk verbruik button not found")
-        browser.quit()
+        print(notFound(login))
+        b1.quit()
 
     try:
-        el = WebDriverWait(browser, 20).until(
-            EC.presence_of_element_located(
-                (
-                    By.CSS_SELECTOR,
-                    "#ro1co1ce1 .cell .bundle-usage-table tr:first-of-type td:nth-last-of-type(2)",
-                )
-            )
-        )
+        b1.find_element_by_id(username_field).send_keys(username)
     except:
-        print("Bundle Status page not loading")
-        browser.quit()
+        print(notFound(username_field))
+        b1.quit()
 
-    bundleStatusElement = browser.find_element_by_css_selector(
-            "#ro1co1ce1 .cell .bundle-usage-table tr:first-of-type td:nth-last-of-type(2)"
-    ).get_attribute("innerHTML")
-    bundleStatusValue = int(re.sub("[\D*]", "", bundleStatusElement))
+    try:
+        b1.find_element_by_id(password_field).send_keys(password)
+    except:
+        print(notFound(password_field))
+        b1.quit()
 
-    while bundleStatusValue >= MB_MINIMUM:
-        print(bundleStatusValue)
-        browser.find_element_by_id("RefreshUsageButton").click()
-        time.sleep(REFRESHRATE)
-        bundleStatusElement = browser.find_element_by_css_selector("#ro1co1ce1 .cell .bundle-usage-table tr:first-of-type td:nth-last-of-type(2)").get_attribute("innerHTML")
-        bundleStatusValue = int(re.sub("[\D*]", "", bundleStatusElement))
-        time.sleep(5)
-        # i+=1 
+    try:
+        b1.find_element_by_id(confirm).click()
+    except:
+        print(notFound(confirm))
+        b1.quit()
 
-        # if i > 5:
-            # reset()
+    try:
+        b1.find_element_by_id(statistic).click()
+    except:
+        print(notFound(statistic))
+        b1.quit()
 
-        
-    mbsAanvullen()
+    return b1
 
-i = 0
-browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
-browser.get(LOGIN)
-browser.maximize_window()
 
-try:
-    browser.find_element_by_css_selector(
-        "label[for=Row1_Column1_Cell1_CookieSettings_SelectedCookieTypeAnalytics]"
-    ).click()
-except:
-    print("Cookie notice button not found")
-    browser.quit()
+def clearHistory(b1):
+    clear_history = "button_clear_history"
+    confirm = "pop_confirm"
 
-try:
-    browser.find_element_by_id("Section1_Row2_Column1_Cell1_AutoLogin_AutoLoginButton").click()
-except:
-    print("Auto Login button not found")
-    browser.quit()
-else:
-    bekijkVerbruik()
+    try:
+        b1.find_element_by_id(clear_history).click()
+    except:
+        print(notFound(clear_history))
+        b1.quit()
+
+    try:
+        b1.find_element_by_id(confirm).click()
+    except:
+        print(notFound(confirm))
+        b1.quit()
+
+
+def readVolume(b1):
+    current_volume = "mobile_current_volume"
+
+    try:
+        b1.find_element_by_id(current_volume).get_attribute("innerHTML")
+    except:
+        print(notFound(current_volume))
+        b1.quit()
+
+    volume = re.sub("[\D*]", "", current_volume)
+
+    return int(volume)
+
+
+def init():
+    b1 = loginMifi()
+    time.sleep(3)
+    clearHistory(b1)
+    volume = readVolume(b1)
+
+    while True:
+        while volume <= MB_MAX:
+            print("Current Volume: " + readVolume())
+            time.sleep(REFRESHRATE)
+            volume = readVolume(b1)
+
+        if mbsAanvullen():
+            clearHistory(b1)
+
+
+init()
